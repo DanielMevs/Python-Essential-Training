21e116ffd9b76eef9ae69641a8d4dcc6286b7069
DanielMevs <dannymevs@hotmail.com>
2024-07-05 20:02:53 -0400

    completed challenge 5 implement square function

---
+++ challenge5.py
@@ -0,0 +1,14 @@
+# Python code​​​​​​‌​‌‌​‌​‌‌‌‌​‌​​‌‌‌‌‌​​‌​‌ below
+def triangle(num):
+    if num == 1:
+        return num
+    return num + triangle(num - 1)
+
+def square(num):
+    # - Get first half of triangle
+    result = triangle(num)
+
+    # - Get second half of triangle
+    for i in range(num, 0, -1):
+        result += num - i
+    return result
